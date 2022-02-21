9966c7a52e3bbb71222dbcd56871e03601a771a2
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-21 11:11:32 +0100

    Show posts on homepage

--- src/sites/Homepage.js
+++ src/sites/Homepage.js
@@ -1,13 +1,47 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import { nanoid } from 'nanoid'
 
 
 const Homepage = (props) => {
 
+    const [postList, setPostList] = useState([]);
+    
+    useEffect(() => {
+        const getPosts = () => {
+
+        fetch('https://calm-wave-71314.herokuapp.com/api/posts', { mode: 'cors' })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+            
+            
+                setPostList(response);
+                
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+      
+        
+        };
+        getPosts();
+        
+    },[])
+    
+   
+    
+    const listItems = postList.map((post) =>
+        <li key={nanoid()}>
+      {post.title}
+    </li>
+  );
+  
 
 
     return (
         <div className="homepage">
-            <h1>All Posts</h1>
+            <ul>{listItems}</ul>
         </div>
         
     );
