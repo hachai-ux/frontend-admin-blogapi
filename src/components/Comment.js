1131383ba04788744e9ffc14e0fe7b52ad0bbfcc
Hac Hai Pham <hpham.ux@gmail.com>
2022-02-22 09:43:18 +0100

    Update comment on database

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useRef} from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 //single comment component
 const Comment = (props) => {
 
+    const editorTextRef = useRef(null);
+    let params = useParams();
+    let postid = params.postid;
+    let navigate = useNavigate();
+
     const [editActive, setEditActive] = useState(false);
+      const [commentText, setCommentText] = useState(props.comment.text);
 
     const editComment = (comment, e) => {
 
@@ -17,6 +25,33 @@ const Comment = (props) => {
     
     const updateComment = (comment, e) => {
 
+          const data = {
+           name: comment.name,
+           text: editorTextRef.current.getContent().replace(/<\/?[^>]+(>|$)/g, "")
+
+          }
+        const dataJSON = JSON.stringify(data);
+        
+        fetch(`https://calm-wave-71314.herokuapp.com/api/posts/${postid}/comments/${comment._id}`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: dataJSON
+                        })
+                            .then(response => response.json())
+                            .then(result => {
+                                
+                                console.log('Success:', result);
+                                setCommentText(editorTextRef.current.getContent().replace(/<\/?[^>]+(>|$)/g, ""));
+                                setEditActive(false);
+                            
+
+                            })
+                            .catch(error => {
+                                console.error('Error:', error);
+                            });
+
     };
 
 
@@ -24,7 +59,7 @@ const Comment = (props) => {
         return (
             <>
                 <h2>{props.comment.name}</h2>
-                <p>{props.comment.text}</p>
+                <p>{commentText}</p>
                 <p>{props.comment.timestamp}</p>
                 <button onClick={(e) => editComment(props.comment, e)}>Edit Comment</button>
                 <button onClick={(e) => deleteComment(props.comment, e)}>Delete Comment</button>
@@ -35,8 +70,27 @@ const Comment = (props) => {
     const EditableComment = (props) => {
         return (
             <>
-                <h2>{props.comment.name}</h2>
-                <p>{props.comment.text}</p>
+                 <h2>{props.comment.name}</h2>
+                  
+                <Editor apiKey='rhl5vp4qavwbk8r93xd01a47m0aaqzkh4a4735w0pd87100r'
+         onInit={(evt, editor) => editorTextRef.current = editor} 
+         initialValue={`${commentText}`}
+         init={{
+           height: 100,
+           menubar: false,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount'
+           ],
+             toolbar: false,
+             statusbar: false,
+             inline: true,
+            auto_focus: true,
+           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+         }}
+       />
+               
                 <p>{props.comment.timestamp}</p>
                 <button onClick={(e) => updateComment(props.comment, e)}>Update Comment</button>
             </>
